3af2bf63d33a9e2cb38e0e673ad01fc3382d8fc5
hakeem235 <hakeemkoo@gmail.com>
2023-01-20 19:37:29 -0500

    update the file

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -11,8 +11,8 @@ SELECT e.emp_no,
 	t.from_date,
 	t.to_date
 INTO retirement_titles
-FROM employees as e
-    INNER JOIN titles as t
+FROM employees AS e
+    INNER JOIN titles AS t
 	ON (e.emp_no = t.emp_no)
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no
@@ -23,13 +23,13 @@ SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
 	rt.last_name,
 	rt.title
 INTO unique_titles
-FROM retirement_titles as rt
+FROM retirement_titles AS rt
 ORDER BY rt.emp_no, rt.to_date DESC;
 
 -- Creating retiring_titles.csv
 SELECT COUNT(ut.emp_no), ut.title
 INTO retiring_titles
-FROM unique_titles as ut
+FROM unique_titles AS ut
 GROUP BY ut.title
 ORDER BY COUNT (ut.emp_no) DESC;
 
@@ -44,10 +44,10 @@ SELECT DISTINCT ON (e.emp_no) e.emp_no,
     de.to_date,
     t.title
 INTO mentorship_eligibility
-FROM employees as e
-    INNER JOIN dept_emp as de
+FROM employees AS e
+    INNER JOIN dept_emp AS de
     ON (e.emp_no = de.emp_no)
-    INNER JOIN titles as t
+    INNER JOIN titles AS t
     ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 	AND (de.to_date = '9999-01-01')
